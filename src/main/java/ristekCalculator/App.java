a2964817613e4ce59248b2969972c97a653a541a
Muhammad Jilham Luthfi <jilham.luthfi15@gmail.com>
2019-11-02 17:12:04 +0700

    [GREEN] add addition function

--- src/main/java/ristekCalculator/App.java
+++ src/main/java/ristekCalculator/App.java
@@ -8,6 +8,10 @@ public class App {
         return "Hello world.";
     }
 
+    public int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
